8bad4e7be805214ce37795dfb06908faae1bd996
Chaos0103 <lyt1228@naver.com>
2024-03-26 00:33:42 +0900

    feat(#LM-29): 일기장 갯수 조회 구현

--- src/main/java/com/lovememoir/server/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/lovememoir/server/domain/diary/repository/DiaryRepository.java
@@ -2,8 +2,13 @@ package com.lovememoir.server.domain.diary.repository;
 
 import com.lovememoir.server.domain.diary.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    @Query("select count(d.id) from Diary d where d.isDeleted = false and d.member.id = :memberId")
+    int countByMemberId(@Param("memberId") Long memberId);
 }
---
+++ src/test/java/com/lovememoir/server/domain/diary/repository/DiaryRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.lovememoir.server.domain.diary.repository;
+
+import com.lovememoir.server.IntegrationTestSupport;
+import com.lovememoir.server.domain.diary.Diary;
+import com.lovememoir.server.domain.member.Gender;
+import com.lovememoir.server.domain.member.Member;
+import com.lovememoir.server.domain.member.Role;
+import com.lovememoir.server.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DiaryRepositoryTest extends IntegrationTestSupport {
+
+    @Autowired
+    private DiaryRepository diaryRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("회원이 등록한 일기장 갯수를 조회한다.")
+    @Test
+    void countByMemberId() {
+        //given
+        Member member = createMember();
+        Diary diary = createDiary(member, false);
+        Diary removedDiary = createDiary(member, true);
+
+        //when
+        int diaryCount = diaryRepository.countByMemberId(member.getId());
+
+        //then
+        assertThat(diaryCount).isOne();
+    }
+
+    private Member createMember() {
+        Member member = Member.builder()
+            .memberKey(UUID.randomUUID().toString())
+            .nickname("아이바오")
+            .gender(Gender.F)
+            .birth("2013-07-13")
+            .role(Role.USER)
+            .build();
+        return memberRepository.save(member);
+    }
+
+    private Diary createDiary(Member member, boolean isDeleted) {
+        Diary diary = Diary.builder()
+            .isFixed(false)
+            .title("러바오와의 연애 기록")
+            .relationshipStartedDate(LocalDate.of(2016, 3, 3))
+            .pageCount(0)
+            .member(member)
+            .isDeleted(isDeleted)
+            .build();
+        return diaryRepository.save(diary);
+    }
+}
