dcd835c665ed7f1016295313dc678dfdbec2ae20
Chaos0103 <lyt1228@naver.com>
2024-04-06 16:57:48 +0900

    test(#LM-65): 일기장 데이터 유효성 검증 테스트 구현

--- src/main/java/com/lovememoir/server/api/service/diary/DiaryValidator.java
+++ src/main/java/com/lovememoir/server/api/service/diary/DiaryValidator.java
@@ -1,49 +1,18 @@
 package com.lovememoir.server.api.service.diary;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import com.lovememoir.server.domain.diary.LoveInfo;
 
-import static com.lovememoir.server.common.message.ValidationMessage.*;
+import java.time.LocalDate;
 
 public abstract class DiaryValidator {
 
     private static final int TITLE_MAXIMUM_LENGTH = 8;
 
     public static String validateTitle(String title) {
-        title = removeSpace(title);
-
-        if (isTitleLengthGraterThan(title)) {
-            throw new IllegalArgumentException(MAX_LENGTH_DIARY_TITLE);
-        }
-
-        return title;
+        return null;
     }
 
-    public static LocalDate validateRelationshipStartedDate(boolean isInLove, final LocalDate relationshipStartedDate, final LocalDateTime currentDateTime) {
-        if (!isInLove) {
-            return relationshipStartedDate;
-        }
-
-        if (relationshipStartedDate == null) {
-            throw new IllegalArgumentException(NOT_NULL_RELATIONSHIP_STARTED_DATE);
-        }
-
-        if (isFuture(currentDateTime, relationshipStartedDate)) {
-            throw new IllegalArgumentException(FUTURE_RELATIONSHIP_STARTED_DATE);
-        }
-
-        return relationshipStartedDate;
-    }
-
-    private static String removeSpace(final String text) {
-        return text.strip();
-    }
-
-    private static boolean isTitleLengthGraterThan(final String title) {
-        return title.length() > TITLE_MAXIMUM_LENGTH;
-    }
-
-    private static boolean isFuture(final LocalDateTime currentDateTime, final LocalDate relationshipStartedDate) {
-        return currentDateTime.isBefore(relationshipStartedDate.atStartOfDay());
+    public static LoveInfo validateLoveInfo(boolean isLove, final LocalDate startedDate, final LocalDate finishedDate, final LocalDate currentDate) {
+        return null;
     }
 }
