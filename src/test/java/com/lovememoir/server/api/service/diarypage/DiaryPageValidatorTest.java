e078f08dd97947b20381bdbbc76092552829f6a5
Chaos0103 <lyt1228@naver.com>
2024-04-06 23:53:17 +0900

    test(#LM-66): 일기 데이터 검증 테스트 코드 구현

--- src/test/java/com/lovememoir/server/api/service/diarypage/DiaryPageValidatorTest.java
+++ src/test/java/com/lovememoir/server/api/service/diarypage/DiaryPageValidatorTest.java
@@ -4,20 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-import static com.lovememoir.server.common.message.ValidationMessage.FUTURE_DIARY_DATE;
-import static com.lovememoir.server.common.message.ValidationMessage.MAX_LENGTH_DIARY_PAGE_TITLE;
+import static com.lovememoir.server.common.message.ValidationMessage.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DiaryPageValidatorTest {
 
-    @DisplayName("입력 받은 제목의 길이가 10자를 초과하면 예외가 발생한다.")
+    @DisplayName("입력 받은 제목의 길이가 20자리를 초과하면 예외가 발생한다.")
     @Test
     void validateTitleWithOutOfLength() {
         //given
-        String title = "푸바오 닮은 루이바오";
+        String title = "푸바오, 루이바오, 후이바오 딸 세마리";
 
         //when //then
         assertThatThrownBy(() -> DiaryPageValidator.validateTitle(title))
@@ -25,56 +23,44 @@ class DiaryPageValidatorTest {
             .hasMessage(MAX_LENGTH_DIARY_PAGE_TITLE);
     }
 
-    @DisplayName("입력 받은 제목의 앞 뒤 공백은 글자수에 포함되지 않는다.")
-    @Test
-    void validateTitleWithSpace() {
-        //given
-        String title = " 말썽쟁이 후이바오!    ";
-
-        //when
-        String validatedTitle = DiaryPageValidator.validateTitle(title);
-
-        //then
-        assertThat(validatedTitle).isEqualTo("말썽쟁이 후이바오!");
-    }
-
-    @DisplayName("입력 받은 제목에 대해 유효성 검증된 제목을 반환한다.")
+    @DisplayName("입력 받은 제목의 데이터 유효성 검증을 한다.")
     @Test
     void validateTitle() {
         //given
-        String title = "말썽쟁이 후이바오!";
+        String title = "중국으로 여행을 떠난 우리 푸바오ㅜㅜ";
 
         //when
         String validatedTitle = DiaryPageValidator.validateTitle(title);
 
         //then
-        assertThat(validatedTitle).isEqualTo("말썽쟁이 후이바오!");
+        assertThat(validatedTitle).isEqualTo(title);
     }
 
-    @DisplayName("입력 받은 연애 시작일이 미래라면 예외가 발생한다.")
+    @DisplayName("입력 받은 기록일이 현재 날짜보다 미래라면 예외가 발생한다.")
     @Test
-    void validateDiaryDateWithFuture() {
+    void validateRecordDateIsFuture() {
         //given
-        LocalDateTime currentDateTime = LocalDateTime.of(2024, 3, 10, 23, 59);
-        LocalDate diaryDate = LocalDate.of(2024, 3, 11);
+        LocalDate currentDate = LocalDate.of(2024, 4, 6);
+        LocalDate recordDate = LocalDate.of(2024, 4, 7);
+
 
         //when //then
-        assertThatThrownBy(() -> DiaryPageValidator.validateDiaryDate(currentDateTime, diaryDate))
+        assertThatThrownBy(() -> DiaryPageValidator.validateRecordDate(recordDate, currentDate))
             .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage(FUTURE_DIARY_DATE);
+            .hasMessage(IS_FUTURE_DATE);
     }
 
-    @DisplayName("입력 받은 연애 시작일에 대해 유효성 검증된 연애 시작일을 반환한다.")
+    @DisplayName("입력 받은 기록일의 데이터 유효성 검증을 한다.")
     @Test
-    void validateDiaryDate() {
+    void validateRecordDate() {
         //given
-        LocalDateTime currentDateTime = LocalDateTime.of(2024, 3, 10, 0, 0);
-        LocalDate diaryDate = LocalDate.of(2024, 3, 10);
+        LocalDate currentDate = LocalDate.of(2024, 4, 6);
+        LocalDate recordDate = LocalDate.of(2024, 4, 6);
 
         //when
-        LocalDate validatedDiaryDate = DiaryPageValidator.validateDiaryDate(currentDateTime, diaryDate);
+        LocalDate validatedRecordDate = DiaryPageValidator.validateRecordDate(recordDate, currentDate);
 
         //then
-        assertThat(validatedDiaryDate).isEqualTo(LocalDate.of(2024, 3, 10));
+        assertThat(validatedRecordDate).isEqualTo(LocalDate.of(2024, 4, 6));
     }
 }
