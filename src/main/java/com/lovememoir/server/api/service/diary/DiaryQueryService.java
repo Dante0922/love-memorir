c25d0d98a93b82e2959142e0c11b40951237953b
Chaos0103 <lyt1228@naver.com>
2024-04-06 22:32:41 +0900

    feat(#LM-65): 일기장 목록 조회 구현

--- src/main/java/com/lovememoir/server/api/service/diary/DiaryQueryService.java
+++ src/main/java/com/lovememoir/server/api/service/diary/DiaryQueryService.java
@@ -2,11 +2,16 @@ package com.lovememoir.server.api.service.diary;
 
 import com.lovememoir.server.domain.diary.repository.DiaryQueryRepository;
 import com.lovememoir.server.domain.diary.repository.response.DiarySearchResponse;
+import com.lovememoir.server.domain.member.Member;
+import com.lovememoir.server.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import static com.lovememoir.server.common.message.ExceptionMessage.NO_SUCH_MEMBER;
 
 @RequiredArgsConstructor
 @Service
@@ -14,8 +19,19 @@ import java.util.List;
 public class DiaryQueryService {
 
     private final DiaryQueryRepository diaryQueryRepository;
+    private final MemberRepository memberRepository;
 
     public List<DiarySearchResponse> searchDiaries(final String providerId) {
-        return null;
+        Member member = memberRepository.findByProviderId(providerId)
+            .orElseThrow(() -> new NoSuchElementException(NO_SUCH_MEMBER));
+        List<DiarySearchResponse> content = diaryQueryRepository.findAllByMemberId(member.getId(), true);
+        List<DiarySearchResponse> diaryContents = diaryQueryRepository.findAllByMemberId(member.getId(), false);
+
+        if (content.isEmpty()) {
+            return diaryContents;
+        }
+
+        content.addAll(diaryContents);
+        return content;
     }
 }
