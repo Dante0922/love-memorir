3490fbf996a6a967612dd2923b666d899a2cc78e
gw8413 <gw8413@gmail.com>
2024-04-24 17:17:40 +0900

    feat(#LM-67): avatarQueryServiceTest 구현

--- src/test/java/com/lovememoir/server/api/controller/avatar/AvatarApiControllerTest.java
+++ src/test/java/com/lovememoir/server/api/controller/avatar/AvatarApiControllerTest.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class AvatarApiControllerTest extends ControllerTestSupport {
@@ -21,19 +24,24 @@ class AvatarApiControllerTest extends ControllerTestSupport {
     @Test
     void refreshAvatar() throws Exception {
         //given
-        AvatarRefreshResponse request = AvatarRefreshResponse.builder()
-                .emotion(Emotion.STABILITY)
-                .question("오늘은 무슨 일이 있었나요?")
-                .build();
+        String providerId = "user011";
+        AvatarRefreshResponse response = AvatarRefreshResponse.builder()
+            .emotion(Emotion.STABILITY)
+            .question("오늘은 무슨 일이 있었나요?")
+            .build();
+        when(avatarService.refreshAvatar(providerId)).thenReturn(response);
+
         //when //then
         mockMvc.perform(
-                        post(BASE_URL + "/refresh")
-                                .content(objectMapper.writeValueAsString(request))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .with(csrf())
-                )
-                .andDo(print())
-                .andExpect(status().isOk());
+                post(BASE_URL + "/refresh")
+                    .with(user(providerId))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .with(csrf())
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.emotion").value("STABILITY"))
+            .andExpect(jsonPath("$.data.question").value("오늘은 무슨 일이 있었나요?"));
     }
 
 }
---
+++ src/test/java/com/lovememoir/server/api/service/avatar/AvatarServiceTest.java
@@ -0,0 +1,7 @@
+package com.lovememoir.server.api.service.avatar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AvatarServiceTest {
+
+}
