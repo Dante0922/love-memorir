bb296dbb5d5d0f65e339c99a5e249aec1f604670
gw8413 <gw8413@gmail.com>
2024-06-18 00:15:31 +0900

    feature(#LM-70): OpenAI API 호출방식 제작_API 응답 성공

--- src/main/java/com/lovememoir/server/api/service/diaryanalysis/OpenAiApiService.java
+++ src/main/java/com/lovememoir/server/api/service/diaryanalysis/OpenAiApiService.java
@@ -18,8 +18,7 @@ public class OpenAiApiService {
 
     private static final String API_URL = "https://api.openai.com/v1/chat/completions";
     private static final String API_KEY = "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";
-//    private static final String API_KEY = "sk-lovelog-eimjvAugdrS2eYBNN8fuT3BlbkFJ2GSk8Hu1inKp06B973Ja";
-//    private static final String API_KEY = "sk-proj-GnntZyDaaoJcj0bhfJqTT3BlbkFJF1Cnc3iQcYgIsWMXZQsw";
+//    private static final String API_KEY = "sk-proj-seQTzUShchYUjxtkXaj7T3BlbkFJjLlw0ymYCdwZ1VVIizoM";
 
     private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
@@ -33,34 +32,35 @@ public class OpenAiApiService {
         ObjectNode requestBody = objectMapper.createObjectNode();
 
         ArrayNode messages = requestBody.putArray("messages");
+        requestBody.put("model", "gpt-4o");
+
         ObjectNode systemMessage = messages.addObject();
         systemMessage.put("role", "system");
         systemMessage.put("content", "단답으로 대답해");
-
         ObjectNode userMessage = messages.addObject();
         userMessage.put("role", "user");
-        userMessage.put("content", "지피티야 안녕?");
-//        requestBody.put("content", prompt);
-        requestBody.put("model", "gpt-3.5-turbo-16k");
+        userMessage.put("content", prompt);
+
 
         String requestBodyStr = objectMapper.writeValueAsString(requestBody);
 
         HttpRequest request = HttpRequest.newBuilder()
             .uri(new URI(API_URL))
             .header("Content-Type", "application/json")
-            .header("Authorization", "Bearer " + API_KEY)
+            .header("Authorization", "Bearer sk-proj-seQTzUShchYUjxtkXaj7T3BlbkFJjLlw0ymYCdwZ1VVIizoM")
             .POST(HttpRequest.BodyPublishers.ofString(requestBodyStr))
             .build();
-        log.info("req 확인" + request.headers().toString());
+        log.info("req 확인" + request.headers().map());
 
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        log.info("resp 확인" + response.toString());
+        log.info("resp 확인" + response.headers().map());
+        log.info("resq 바디", response.body().toString());
 
         if (response.statusCode() != 200) {
             throw new RuntimeException("GPT 실패" + response.toString());
         }
 
         ObjectNode responseJson = objectMapper.readValue(response.body(), ObjectNode.class);
-        return responseJson.get("choices").get(0).get("text").asText(); // ??
+        return responseJson.path("choices").get(0).path("message").path("content").asText();
     }
 }
